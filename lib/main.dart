a26b46cef805f0350b01140a471bc0f7c396215a
NinjaBitroom <gabrielbitercourt@gmail.com>
2023-10-31 16:26:25 -0400

    .

--- lib/main.dart
+++ lib/main.dart
@@ -22,47 +22,77 @@ class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.grey,
-          leading: const Icon(
-            Icons.menu,
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        leading: const Icon(
+          Icons.menu,
+          size: 30,
+        ),
+        title: const Text("InfoCar App"),
+        actions: const [
+          Icon(
+            Icons.search,
             size: 30,
           ),
-          title: const Text("InfoCar App"),
-          actions: const [
-            Icon(
-              Icons.search,
-              size: 30,
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Card(
+              color: Colors.black12,
+              child: Column(
+                children: [
+                  const ListTile(
+                    title: Text('Marcas'),
+                    trailing: Text('Ver todas'),
+                  ),
+                  GridView.count(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2),
+                    children: const [
+                      Text('teste'),
+                      Text('teste'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const Card(
+              color: Colors.lightBlueAccent,
+              child: ListTile(
+                title: Text('Carros disponíveis'),
+                subtitle: Text('Confira a lista completa'),
+                trailing: Text('>'),
+              ),
+            ),
+            const Card(
+              color: Colors.black26,
+              child: ListTile(
+                title: Text('Mais acessados'),
+              ),
             ),
           ],
         ),
-        body: const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Card(
-                color: Colors.black12,
-                child: GridTile(
-                  header: Text('Marcas'),
-                  child: Text('Ver todas'),
-                ),
-              ),
-              Card(
-                color: Colors.lightBlueAccent,
-                child: ListTile(
-                  title: Text('Carros disponíveis'),
-                  subtitle: Text('Confira a lista completa'),
-                  trailing: Text('>'),
-                ),
-              ),
-              Card(
-                color: Colors.black26,
-                child: ListTile(
-                  title: Text('Mais acessados'),
-                ),
-              ),
-            ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.directions_car),
+            label: 'Principal',
           ),
-        ));
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border),
+            label: 'Favoritos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: 'Perfil',
+          ),
+        ],
+      ),
+    );
   }
 }
