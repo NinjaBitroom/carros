0a028143124ba7ab3a86b41211d5882169e6e2c1
NinjaBitroom <gabrielbitercourt@gmail.com>
2023-11-06 20:40:03 -0400

    Refatore as páginas

--- lib/main.dart
+++ lib/main.dart
@@ -11,35 +11,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Main(),
+      home: MyHomePage(),
     );
   }
 }
 
-class Main extends StatelessWidget {
-  const Main({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.grey,
-        leading: const Icon(
-          Icons.menu,
-          size: 30,
-        ),
-        title: const Text("InfoCar App"),
-        actions: const [
-          Padding(
-            padding: EdgeInsets.only(right: 8.0),
-            child: Icon(
-              Icons.search,
-              size: 30,
-            ),
-          ),
-        ],
-      ),
-      body: const Padding(
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  List<Widget> pages = const [
+    Padding(
         padding: EdgeInsets.all(8.0),
         child: Column(
           children: [
@@ -198,6 +185,36 @@ class Main extends StatelessWidget {
           ],
         ),
       ),
+    Text("2"),
+    Text("3"),
+  ];
+  void _changeIndex(int value) {
+    setState(() {
+      _selectedIndex = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        leading: const Icon(
+          Icons.menu,
+          size: 30,
+        ),
+        title: const Text("InfoCar App"),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 8.0),
+            child: Icon(
+              Icons.search,
+              size: 30,
+            ),
+          ),
+        ],
+      ),
+      body: Center(child: pages.elementAt(_selectedIndex),),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(
@@ -213,6 +230,8 @@ class Main extends StatelessWidget {
             label: 'Perfil',
           ),
         ],
+        currentIndex: _selectedIndex,
+        onTap: _changeIndex,
       ),
     );
   }
