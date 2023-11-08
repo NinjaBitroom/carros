893e279c5e51dc3121db9dc04bb297b9b1460b8b
NinjaBitroom <gabrielbitercourt@gmail.com>
2023-11-07 21:58:16 -0400

    Refatore o código e melhore a legibilidade

--- lib/pages/carros.dart
+++ lib/pages/carros.dart
@@ -8,6 +8,50 @@ class PageCar extends StatefulWidget {
 }
 
 class _PageCarState extends State<PageCar> {
+  carroItem() => Padding(
+        padding: const EdgeInsets.only(right: 8.0),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+          ),
+          child: const Padding(
+            padding: EdgeInsets.all(12.0),
+            child: Column(
+              children: [
+                Image(
+                  image: AssetImage('images/download.jpeg'),
+                  width: 64,
+                ),
+                Text('INTEGRA'),
+                Text('Acura'),
+                Text('\$31,500+'),
+              ],
+            ),
+          ),
+        ),
+      );
+
+  marcaItem(String titulo) => Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'images/acuralogo.png',
+              width: 48,
+            ),
+            Text(
+              titulo,
+              style: const TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -15,83 +59,50 @@ class _PageCarState extends State<PageCar> {
       child: Column(
         children: [
           Container(
-            height: 200,
             decoration: BoxDecoration(
               color: Colors.black12,
               borderRadius: BorderRadius.circular(12),
             ),
-            child: const Padding(
-              padding: EdgeInsets.all(12.0),
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
               child: Column(
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Marcas',
-                        style: TextStyle(
-                          fontSize: 20,
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Marcas',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
                         ),
-                      ),
-                      Text(
-                        'Ver todas',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.blue,
-                          decorationColor: Colors.blue,
-                          decorationThickness: 2.0,
-                          decoration: TextDecoration.underline,
+                        Text(
+                          'Ver todas',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue,
+                            decorationColor: Colors.blue,
+                            decorationThickness: 2.0,
+                            decoration: TextDecoration.underline,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Image(
-                        image: AssetImage('images/acuralogo.png'),
-                        width: 64,
-                      ),
-                      Image(
-                        image: AssetImage('images/acuralogo.png'),
-                        width: 64,
-                      ),
-                      Image(
-                        image: AssetImage('images/acuralogo.png'),
-                        width: 64,
-                      ),
-                      Image(
-                        image: AssetImage('images/acuralogo.png'),
-                        width: 64,
-                      ),
-                    ],
+                  GridView.count(
+                    crossAxisCount: 4,
+                    crossAxisSpacing: 8,
+                    mainAxisSpacing: 8,
+                    shrinkWrap: true,
+                    children: List.generate(
+                      8,
+                      (index) => marcaItem('Acura'),
+                    ),
                   ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Image(
-                        image: AssetImage('images/acuralogo.png'),
-                        width: 64,
-                      ),
-                      Image(
-                        image: AssetImage('images/acuralogo.png'),
-                        width: 64,
-                      ),
-                      Image(
-                        image: AssetImage('images/acuralogo.png'),
-                        width: 64,
-                      ),
-                      Image(
-                        image: AssetImage('images/acuralogo.png'),
-                        width: 64,
-                      ),
-                    ],
-                  ),
-                  SizedBox(
+                  const SizedBox(
                     height: 10,
                   ),
                 ],
@@ -110,98 +121,19 @@ class _PageCarState extends State<PageCar> {
               )),
             ),
           ),
-          const Card(
+          Container(
             color: Colors.black26,
             child: Padding(
-              padding: EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(8.0),
               child: Column(
                 children: [
-                  ListTile(
-                    title: Text('Mais acessados'),
+                  const Row(
+                    children: [
+                      Text('Mais acessados'),
+                    ],
                   ),
                   Row(
-                    children: [
-                      Card(
-                        child: Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Column(
-                            children: [
-                              Image(
-                                image: AssetImage('images/download.jpeg'),
-                                width: 64,
-                              ),
-                              Text('INTEGRA'),
-                              Text('Acura'),
-                              Text('\$31,500+'),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Card(
-                        child: Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Column(
-                            children: [
-                              Image(
-                                image: AssetImage('images/download.jpeg'),
-                                width: 64,
-                              ),
-                              Text('INTEGRA'),
-                              Text('Acura'),
-                              Text('\$31,500+'),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Card(
-                        child: Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Column(
-                            children: [
-                              Image(
-                                image: AssetImage('images/download.jpeg'),
-                                width: 64,
-                              ),
-                              Text('INTEGRA'),
-                              Text('Acura'),
-                              Text('\$31,500+'),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Card(
-                        child: Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Column(
-                            children: [
-                              Image(
-                                image: AssetImage('images/download.jpeg'),
-                                width: 64,
-                              ),
-                              Text('INTEGRA'),
-                              Text('Acura'),
-                              Text('\$31,500+'),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Card(
-                        child: Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Column(
-                            children: [
-                              Image(
-                                image: AssetImage('images/download.jpeg'),
-                                width: 64,
-                              ),
-                              Text('INTEGRA'),
-                              Text('Acura'),
-                              Text('\$31,500+'),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
+                    children: List.generate(4, (index) => carroItem()),
                   )
                 ],
               ),
